83836cd06a0ab1edbdc263498c889ad908684cbc
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-03-08 21:23:18 +0800

    Update Squares of a Sorted Array.cpp

--- cpp/Squares of a Sorted Array.cpp
+++ cpp/Squares of a Sorted Array.cpp
@@ -38,9 +38,11 @@ class Solution {
 public:
 	vector<int> sortedSquares(vector<int>& A) {
 		for (int i = 0; i < A.size(); i++) {
-			A[i] = A[i] * A[i];  //first step,square each element 
+/* first step,square each element */
+			A[i] = A[i] * A[i];  
 		}
-		sort(A.begin(), A.end(), less<int>());  //second, sort them
+		/* second step, sort them */
+		sort(A.begin(), A.end(), less<int>());  
 		return A;
 	}
 };
