d42e1ba9491ef4fa04d3b796351f828e7138b381
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-03-10 23:25:25 +0800

    Update Detect Capital.cpp

--- cpp/Detect Capital.cpp
+++ cpp/Detect Capital.cpp
@@ -13,20 +13,19 @@ Easy
 
 Share
 Given a word, you need to judge whether the usage of capitals in it is right or not.
-
 We define the usage of capitals in a word to be right when one of the following cases holds:
 
-All letters in this word are capitals, like ¡°USA¡±.
-All letters in this word are not capitals, like ¡°leetcode¡±.
-Only the first letter in this word is capital if it has more than one letter, like ¡°Google¡±.
-Otherwise, we define that this word doesn¡¯t use capitals in a right way.
+All letters in this word are capitals, like â€œUSAâ€.
+All letters in this word are not capitals, like â€œleetcodeâ€.
+Only the first letter in this word is capital if it has more than one letter, like â€œGoogleâ€.
+Otherwise, we define that this word doesnâ€™t use capitals in a right way.
 
 Example 1:
-Input: ¡°USA¡±
+Input: â€œUSAâ€
 Output: True
 
 Example 2:
-Input: ¡°FlaG¡±
+Input: â€œFlaGâ€
 Output: False
 
 Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
@@ -93,7 +92,7 @@ public:
 };
 
 /*
-Submission Detail£º
+Submission Detailï¼š
 
 Runtime: 8 ms
 Memory Usage: 11.4 MB
