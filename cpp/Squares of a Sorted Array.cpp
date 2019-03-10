de7f443b81a12439065e33c2d85fdb29edb7af01
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-03-10 23:30:27 +0800

    Update Squares of a Sorted Array.cpp

--- cpp/Squares of a Sorted Array.cpp
+++ cpp/Squares of a Sorted Array.cpp
@@ -6,17 +6,19 @@ Date: 03-05-2019
 
 
 /*
+
+977. Squares of a Sorted Array 
+
 Easy
 
 Share
 Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
 Example 1:
-
 Input: [-4,-1,0,3,10]
 Output: [0,1,9,16,100]
-Example 2:
 
+Example 2:
 Input: [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
 
@@ -27,6 +29,7 @@ Note:
 A is sorted in non-decreasing order.
 
 */
+
 
 #include <functional>
 #include <iostream>     // std::cout
