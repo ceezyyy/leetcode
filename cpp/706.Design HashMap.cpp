bb0cfe6181baf2e7403996fc0b296b49d94ec054
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-03-28 16:53:11 +0800

    HashMap

---
+++ cpp/706.Design HashMap.cpp
@@ -0,0 +1,85 @@
+/*
+Source: LeetCode 706. Design HashMap
+Author: ceezyyy
+Date: 03-28-2019
+*/
+
+
+/*
+Easy
+
+Share
+Design a HashMap without using any built-in hash table libraries.
+To be specific, your design should include these functions:
+put(key, value) : Insert a (key, value) pair into the HashMap. 
+If the value already exists in the HashMap, update the value.
+get(key): Returns the value to which the specified key is mapped, 
+or -1 if this map contains no mapping for the key.
+remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+
+Example:
+MyHashMap hashMap = new MyHashMap();
+hashMap.put(1, 1);
+hashMap.put(2, 2);
+hashMap.get(1); // returns 1
+hashMap.get(3); // returns -1 (not found)
+hashMap.put(2, 1); // update the existing value
+hashMap.get(2); // returns 1
+hashMap.remove(2); // remove the mapping for 2
+hashMap.get(2); // returns -1 (not found)
+
+Note:
+All keys and values will be in the range of [0, 1000000].
+The number of operations will be in the range of [1, 10000].
+Please do not use the built-in HashMap library.
+
+*/
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class MyHashMap {
+public:
+	/** Initialize your data structure here. */
+	MyHashMap() {
+		data.resize(1000000, -1);
+	}
+
+	/** value will always be non-negative. */
+	void put(int key, int value) {
+		data[key] = value;
+	}
+
+	/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+	int get(int key) {
+		return data[key];
+	}
+
+	/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+	void remove(int key) {
+		data[key] = -1;
+	}
+
+private:
+	vector<int> data;
+};
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
+
+ /*
+ Submission Detail:
+
+ Runtime: 200 ms, faster than 31.31% of C++ online submissions for Design HashMap.
+ Memory Usage: 185.6 MB, less than 25.95% of C++ online submissions for Design HashMap.
+
+ */
