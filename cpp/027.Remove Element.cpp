597301963b56744a59a13aa517a24f3abf63806c
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-03-28 16:23:54 +0800

    Array

--- cpp/027.Remove Element.cpp
+++ cpp/027.Remove Element.cpp
@@ -1,5 +1,5 @@
 /*
-Source: LeetCode 283. Move Zeroes
+Source: LeetCode 027. Remove Element
 Author: ceezyyy
 Date: 03-26-2019
 */
@@ -13,18 +13,18 @@ Given an array nums and a value val, remove all instances of that value in-place
 and return the new length.
 Do not allocate extra space for another array, you must do this by modifying 
 the input array in-place with O(1) extra memory.
-The order of elements can be changed. It doesn¡¯t matter what you leave beyond the new length.
+The order of elements can be changed. It doesnÂ¡Â¯t matter what you leave beyond the new length.
 
 Example 1:
 Given nums = [3,2,2,3], val = 3,
 Your function should return length = 2, with the first two elements of nums being 2.
-It doesn¡¯t matter what you leave beyond the returned length.
+It doesnÂ¡Â¯t matter what you leave beyond the returned length.
 
 Example 2:
 Given nums = [0,1,2,2,3,0,4,2], val = 2,
 Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
 Note that the order of those five elements can be arbitrary.
-It doesn¡¯t matter what values are set beyond the returned length.
+It doesnÂ¡Â¯t matter what values are set beyond the returned length.
 
 Clarification:
 Confused why the returned value is an integer but your answer is an array?
