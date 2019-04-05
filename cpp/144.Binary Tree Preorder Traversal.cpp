c5b1e711e8e0f53ea228d9a056b40ad7575aa33e
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-04-05 20:37:42 +0800

    Binary Tree

--- cpp/144.Binary Tree Preorder Traversal.cpp
+++ cpp/144.Binary Tree Preorder Traversal.cpp
@@ -5,21 +5,24 @@
  */
 
 
-/**
- * Medium
- * Share
- * Given a binary tree, return the preorder traversal of its nodes¡¯ values.
- * Example:
- * Input: [1,null,2,3]
+/*
+Medium
+
+Share
+Given a binary tree, return the preorder traversal of its nodes¡¯ values.
+
+Example:
+Input: [1,null,2,3]
    1
 	\
 	 2
 	/
    3
 
- * Output: [1,2,3]
- * Follow up: Recursive solution is trivial, could you do it iteratively?
- */
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+ 
+*/
 
 
 #include<iostream>
