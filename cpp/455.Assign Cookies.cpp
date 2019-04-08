e104cdb67e99d3f5b3d405938ecd200a25d50f96
ceezyyy <38361866+ceezyyy@users.noreply.github.com>
2019-04-08 14:06:58 +0800

    Greedy

--- cpp/455.Assign Cookies.cpp
+++ cpp/455.Assign Cookies.cpp
@@ -1,8 +1,8 @@
-/*
-Source: LeetCode 455. Assign Cookies
-Author: ceezyyy
-Date: 03-18-2019
-*/
+/**
+ * Source: LeetCode 455. Assign Cookies
+ * Author: ceezyyy
+ * Date: 03-18-2019
+ */
 
 
 /*
@@ -23,6 +23,7 @@ Output: 1
 Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
 And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
 You need to output 1.
+
 Example 2:
 Input: [1,2], [1,2,3]
 Output: 2
@@ -60,10 +61,8 @@ public:
 };
 
 
-/*
-Submission Detail:
-
-Runtime: 44 ms, faster than 68.38% of C++ online submissions for Assign Cookies.
-Memory Usage: 10.2 MB, less than 100.00% of C++ online submissions for Assign Cookies.
-
-*/
+/**
+ * Submission Detail:
+ * Runtime: 44 ms, faster than 68.38% of C++ online submissions for Assign Cookies.
+ * Memory Usage: 10.2 MB, less than 100.00% of C++ online submissions for Assign Cookies.
+ */
