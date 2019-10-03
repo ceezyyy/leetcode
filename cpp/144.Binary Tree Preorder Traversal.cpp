97a30e1f2fdfa7366d4eb3396f64285952506462
Yi Cai <38361866+ceezyyy@users.noreply.github.com>
2019-10-03 14:59:45 +0800

    Binary Tree

--- cpp/144.Binary Tree Preorder Traversal.cpp
+++ cpp/144.Binary Tree Preorder Traversal.cpp
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Source: LeetCode  144. Binary Tree Preorder Traversal
  * Author: ceezyyy
  * Date: 10-03-2019
@@ -38,6 +38,8 @@ using namespace std;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// preorderTraversal_Recursion
 class Solution {
 public:
 	vector<int> preorderTraversal(TreeNode* root) {
@@ -46,7 +48,6 @@ public:
 		return res;
 	}
 
-	//preorderTraversal_Recursion
 	void POT_Recursion(TreeNode *node, vector<int> &r) {
 		if (!node)
 			return;
@@ -60,4 +61,45 @@ public:
 /**
  * Runtime: 4 ms, faster than 58.93% of C++ online submissions for Binary Tree Preorder Traversal.
  * Memory Usage: 9.4 MB, less than 44.83% of C++ online submissions for Binary Tree Preorder Traversal.
+ */
+
+
+// preorderTraversal_Iteration
+class Solution {
+public:
+	vector<int> preorderTraversal(TreeNode* root) {
+		vector<int> res;  // store the result
+		stack<TreeNode*> s_node;
+		if (root) {  // determine the root is empty or not!
+			s_node.push(root);
+			POT_Iteration(s_node, res);
+		}
+		return res;
+	}
+
+	void POT_Iteration(stack<TreeNode*> &s, vector<int> &r) {
+		while (!s.empty()) {
+			TreeNode *node = s.top();
+			r.push_back(node->val);
+			s.pop();
+			if (node->right)  // determine the node is empty or not!
+				s.push(node->right);
+			if (node->left)
+				s.push(node->left);
+		}
+		return;
+	}
+};
+
+
+/**
+ * Runtime: 0 ms, faster than 100.00% of C++ online submissions for Binary Tree Preorder Traversal.
+ * Memory Usage: 9 MB, less than 100.00% of C++ online submissions for Binary Tree Preorder Traversal.
+ */
+
+
+/**
+ * Study Notes:
+ * Determine the node is empty or not
+ * Test the situation that is NULL([])
  */
