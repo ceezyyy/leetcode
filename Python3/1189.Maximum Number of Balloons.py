8e443ed3cf602de479bdea3c6c9abe0f57073351
Yi Cai <38361866+ceezyyy@users.noreply.github.com>
2019-10-22 23:49:28 +0800

    dict

---
+++ Python3/1189.Maximum Number of Balloons.py
@@ -0,0 +1,28 @@
+"""
+1189. Maximum Number of Balloons
+Date: Oct-22 2019
+Author: ceezyyy
+"""
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        aim='balon'
+        # dict.fromkeys() is a classmethod as well as the initialization of dict
+        # d={'b','a','l','o','n'}
+        d=dict.fromkeys(aim,0) 
+        for i in text:
+            if i in d:
+                d[i]=d[i]+1
+        d['l']=int(d['l']/2)  # 'l' appears twice
+        d['o']=int(d['o']/2)  # 'o' appears twice
+        return min(d.values())
+
+
+def main():
+    s=Solution()
+    print(s.maxNumberOfBalloons("loonbalxballpoon"))
+
+
+if __name__ == "__main__":
+    main()
