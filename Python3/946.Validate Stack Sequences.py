41bc1fffe3e073d962d5b5c8c68cc3882aa59cdc
Yi Cai <38361866+ceezyyy@users.noreply.github.com>
2019-10-26 11:02:59 +0800

    stack

---
+++ Python3/946.Validate Stack Sequences.py
@@ -0,0 +1,30 @@
+"""
+946. Validate Stack Sequences
+Date: Oct-26 2019
+Author: ceezyyy
+Difficulty: Medium
+"""
+
+
+import collections
+from typing import List
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        for i in pushed:
+            stack.append(i)
+            while stack and popped and stack[-1] == popped[0]:  # stack top
+                stack.pop()
+                popped.pop(0)
+        return not stack
+
+
+def test():
+    s = Solution()
+    print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
+
+
+if __name__ == "__main__":
+    test()
