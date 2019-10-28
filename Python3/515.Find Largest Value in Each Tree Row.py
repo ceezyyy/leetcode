49d2c081f65b05db4a5a00c7165090fa0990e489
Yi Cai <38361866+ceezyyy@users.noreply.github.com>
2019-10-28 21:29:40 +0800

    binary tree level-order

---
+++ Python3/515.Find Largest Value in Each Tree Row.py
@@ -0,0 +1,38 @@
+"""
+515. Find Largest Value in Each Tree Row
+Date: Oct-28 2019
+Author: ceezyyy
+Difficulty: Medium
+"""
+
+from typing import List
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        # level order
+        res = []
+        if not root:
+            return res
+        queue = [root]
+        while queue:
+            temp = []
+            size = len(queue)
+            for i in range(size):  # each level
+                node = queue.pop(0)  # the front of the queue
+                if(node.left):  # left child
+                    queue.append(node.left)
+                if(node.right):  # right child
+                    queue.append(node.right)
+                temp.append(node.val)
+            res.append(max(temp))
+        return res
