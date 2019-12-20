6ef28c06154394f9f6f784420ecd4c42bf388e0c
Yi Cai <ceezyyy11@gmail.com>
2019-12-20 20:47:06 +0800

    Add files via upload

---
+++ Python3/020.Valid Parentheses.py
@@ -0,0 +1,30 @@
+"""
+@Title: 20. Valid Parentheses
+@Tag: stack
+@Date: Oct-24 2019
+@Author: ceezyyy
+@Difficulty: Easy
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []  # type(stack)->list
+        mapping = {')': '(', '}': '{', ']': '['}  # dict[closing]=opening
+        for c in s:
+            if c in mapping:  # closing bracket
+                top_element = stack.pop() if stack else '#'
+                if(mapping[c] != top_element):
+                    return False
+            else:
+                stack.append(c)  # opening bracket
+        return not stack
+
+
+def test():
+    s = Solution()
+    print(s.isValid("(()[]{}"))
+
+
+if __name__ == "__main__":
+    test()
