b5ddcbb1ca941c27e8acf911f7bf15605543002e
Yi Cai <ceezyyy11@gmail.com>
2020-01-23 11:11:03 +0800

    Add files via upload

--- Python3/412.Fizz Buzz.py
+++ Python3/412.Fizz Buzz.py
@@ -11,22 +11,20 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
         for i in range(1, n+1):
-            if i >= 3 and i % 3 == 0 and i >= 5 and i % 5 == 0:
+            if i % 3 == 0 and i % 5 == 0:
                 res.append("FizzBuzz")
-                continue
-            if i >= 3 and i % 3 == 0:
+            elif i % 3 == 0:
                 res.append("Fizz")
-                continue
-            if i >= 5 and i % 5 == 0:
+            elif i % 5 == 0:
                 res.append("Buzz")
-                continue
-            res.append(str(i))
+            else:
+                res.append(str(i))
         return res
 
 
 """
-Runtime: 68 ms, faster than 5.61% of Python3 online submissions for Fizz Buzz.
-Memory Usage: 13.7 MB, less than 100.00% of Python3 online submissions for Fizz Buzz.
+Runtime: 52 ms, faster than 8.70% of Python3 online submissions for Fizz Buzz.
+Memory Usage: 13.5 MB, less than 100.00% of Python3 online submissions for Fizz Buzz.
 """
 
 
