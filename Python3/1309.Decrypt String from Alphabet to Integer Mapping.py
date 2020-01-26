abd431b0e7a7c6fbd3c3215fee5e3cff1825b2f6
Yi Cai <ceezyyy11@gmail.com>
2020-01-26 13:06:04 +0800

    Add files via upload

--- Python3/1309.Decrypt String from Alphabet to Integer Mapping.py
+++ Python3/1309.Decrypt String from Alphabet to Integer Mapping.py
@@ -9,15 +9,15 @@
 
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        res, i = [], len(s) - 1  # backwards
+        res, i = [], len(s) - 1  # res: list[str], i: pointer
         while i >= 0:  # here "while" instead of "for" in order to better move our pointer
-            if s[i] == "#":
+            if s[i] == "#":  # check s[i-2:i]
                 res.append(chr(97 + int(s[i-2:i]) - 1))  # mapping
                 i -= 3  # backwards
             else:
                 res.append(chr(97 + int(s[i]) - 1))  # mapping
                 i -= 1  # backwards
-        return "".join(res[::-1])
+        return "".join(res[::-1])  # convert list[str] to str
 
 
 """
