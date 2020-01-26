5e4250f79988c45fab917f936cb850ed132f24bf
Yi Cai <ceezyyy11@gmail.com>
2020-01-26 22:26:40 +0800

    Add files via upload

--- Python3/344.Reverse String.py
+++ Python3/344.Reverse String.py
@@ -7,6 +7,8 @@
 """
 
 
+# Solution one:
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
@@ -18,4 +20,31 @@ class Solution:
 """
 Runtime: 204 ms, faster than 93.45% of Python3 online submissions for Reverse String.
 Memory Usage: 17.3 MB, less than 95.35% of Python3 online submissions for Reverse String.
+"""
+
+
+# Solution 2: Two pointers
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
+
+
+"""
+Runtime: 248 ms, faster than 9.41% of Python3 online submissions for Reverse String.
+Memory Usage: 17.1 MB, less than 100.00% of Python3 online submissions for Reverse String.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
 """
