52caa1167b4eea62b170f77f5f06986504a1c5e7
Yi Cai <ceezyyy11@gmail.com>
2020-01-27 10:56:51 +0800

    Add files via upload

--- Python3/125.Valid Palindrome.py
+++ Python3/125.Valid Palindrome.py
@@ -7,6 +7,9 @@
 """
 
 
+# Solution one:
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(ch for ch in s if ch not in set(
@@ -17,4 +20,36 @@ class Solution:
 """
 Runtime: 180 ms, faster than 5.44% of Python3 online submissions for Valid Palindrome.
 Memory Usage: 14.1 MB, less than 51.19% of Python3 online submissions for Valid Palindrome.
+"""
+
+
+# Solution two: Two pointers
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1  # two pointers
+        while left < right:
+            if s[left].isalnum() and s[right].isalnum():
+                if s[left].lower() != s[right].lower():  # ignoring cases
+                    return False
+                else:
+                    left += 1
+                    right -= 1
+            elif not s[left].isalnum():  # left is not a alphanumeric
+                left += 1
+            else:  # right is not a alphanumeric
+                right -= 1
+        return True
+
+
+"""
+Runtime: 68 ms, faster than 13.98% of Python3 online submissions for Valid Palindrome.
+Memory Usage: 13.3 MB, less than 76.19% of Python3 online submissions for Valid Palindrome.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
 """
