6cf297b4d823a9ec65f38b6722123258b38ec630
Yi Cai <ceezyyy11@gmail.com>
2020-02-05 16:40:07 +0800

    Add files via upload

--- Python3/1013.Partition Array Into Three Parts With Equal Sum.py
+++ Python3/1013.Partition Array Into Three Parts With Equal Sum.py
@@ -9,25 +9,26 @@
 
 class Solution:
     def canThreePartsEqualSum(self, A: List[int]) -> bool:
-        temp, k, n = 0, sum(A) // 3, len(A)  # "k" -> target sum
+        # left boundary, right boundary, target sum, total number of elements
+        left, right, temp, k, n = 0, 0, 0, sum(A) // 3, len(A)
         for i, a in enumerate(A):  # left part (forward)
             temp += a
             if temp == k:
                 left = i  # the position of the left part
                 break
         temp = 0  # reset
-        for j in range(n - 1, -1, -1):  # right part (backward)
-            temp += A[j]
+        for j, a in reversed(list(enumerate(A))):  # right part (backward)
+            temp += a
             if temp == k:
                 right = j  # the position of the right part
                 break
-        # "sum(A[left + 1:right])" -> the middle part
+        # "sum(A[left+1:right])" -> the middle part
         return sum(A[left + 1:right]) == k and left < right
 
 
 """
-Runtime: 336 ms, faster than 72.96% of Python3 online submissions for Partition Array Into Three Parts With Equal Sum.
-Memory Usage: 19.3 MB, less than 6.25% of Python3 online submissions for Partition Array Into Three Parts With Equal Sum.
+Runtime: 320 ms, faster than 97.47% of Python3 online submissions for Partition Array Into Three Parts With Equal Sum.
+Memory Usage: 21 MB, less than 6.25% of Python3 online submissions for Partition Array Into Three Parts With Equal Sum.
 """
 
 
