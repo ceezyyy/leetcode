fb534b5eb4400c64eeddf6802cab70249e38860b
Yi Cai <ceezyyy11@gmail.com>
2020-02-09 13:24:29 +0800

    Add files via upload

---
+++ Python3/1346.Check If N and Its Double Exist.py
@@ -0,0 +1,32 @@
+"""
+@Title: 1346. Check If N and Its Double Exist
+@Tag: array
+@Date: Feb-09 2020
+@Author: ceezyyy
+@Difficulty: Easy
+"""
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        arr = sorted(arr)
+        print(arr)
+        for a in arr:
+            if a < 0:
+                if a / 2 in arr:
+                    return True
+            if a > 0:
+                if a * 2 in arr:
+                    print("2")
+                    return True
+            if a == 0:
+                if arr.count(0) >= 2:
+                    print("3")
+                    return True
+        return False
+
+
+"""
+Time Complexity: O(nlogn)
+Space Complexity: O(1)
+"""
