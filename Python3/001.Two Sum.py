681aa954d5115e33fd2b5d9f80d7093add9314a9
Yi Cai <ceezyyy11@gmail.com>
2020-02-18 10:26:09 +0800

    Add files via upload

--- Python3/001.Two Sum.py
+++ Python3/001.Two Sum.py
@@ -1,13 +1,13 @@
 """
 @Title: 001. Two Sum
-@Tag: greedy
+@Tag: hashmap
 @Date: Feb-18 2020
 @Author: ceezyyy
 @Difficulty: Easy
 """
 
 
-# Solution one:
+# Solution one(brute force):
 
 
 class Solution:
@@ -30,4 +30,26 @@ Memory Usage: 13.6 MB, less than 96.98% of Python3 online submissions for Two Su
 """
 
 
-# Solution two:
+# Solution two(hashmap):
+
+
+class Solution:
+    def twoSum(self, a: List[int], target: int) -> List[int]:
+        d = {}  # dict
+        for i in range(len(a)):
+            temp = target - a[i]
+            if temp in d:
+                return [i, d[temp]]
+            d[a[i]] = i
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+
+"""
+Runtime: 48 ms, faster than 79.17% of Python3 online submissions for Two Sum.
+Memory Usage: 14.1 MB, less than 62.09% of Python3 online submissions for Two Sum.
+"""
