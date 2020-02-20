0cba53b697ef6629a4a5f154e64e8ff8497c9dcf
Yi Cai <ceezyyy11@gmail.com>
2020-02-20 12:02:55 +0800

    Add files via upload

--- Python3/260.Single Number III.py
+++ Python3/260.Single Number III.py
@@ -7,6 +7,12 @@
 """
 
 
+import collections
+
+
+# Solution one(sort & one pass)
+
+
 class Solution:
     def singleNumber(self, N: List[int]) -> List[int]:
         N, n, res = sorted(N), len(N), []
@@ -29,4 +35,30 @@ Memory Usage: 14.4 MB, less than 100.00% of Python3 online submissions for Singl
 """
 Time Complexity: O(nlogn)
 Space Complexity: O(1)
+"""
+
+
+# Solution two(hashmap & two pass)
+
+
+class Solution:
+    def singleNumber(self, N: List[int]) -> List[int]:
+        res, d = [], collections.defaultdict(int)
+        for n in N:
+            d[n] += 1
+        for n in N:
+            if d[n] == 1:
+                res.append(n)
+        return res
+
+
+"""
+Runtime: 60 ms, faster than 72.33% of Python3 online submissions for Single Number III.
+Memory Usage: 14.5 MB, less than 100.00% of Python3 online submissions for Single Number III.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
