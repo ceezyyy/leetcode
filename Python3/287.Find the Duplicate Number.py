cbcd53c1065ad8d70146fdbc9e39da1302fc451a
Yi Cai <ceezyyy11@gmail.com>
2020-02-22 18:54:30 +0800

    Add files via upload

--- Python3/287.Find the Duplicate Number.py
+++ Python3/287.Find the Duplicate Number.py
@@ -10,7 +10,7 @@
 import collections
 
 
-# Solution one(hashmap)
+# Solution one(hashmap):
 
 
 class Solution:
@@ -31,4 +31,27 @@ Memory Usage: 15.1 MB, less than 17.86% of Python3 online submissions for Find t
 """
 Time Complexity: O(n)
 Space Complexity: O(n)
+"""
+
+
+# Solution two(sort):
+
+
+class Solution:
+    def findDuplicate(self, A: List[int]) -> int:
+        A, n = sorted(A), len(A)
+        for i in range(1, n):
+            if A[i] == A[i-1]:
+                return A[i]
+
+
+"""
+Runtime: 68 ms, faster than 64.72% of Python3 online submissions for Find the Duplicate Number.
+Memory Usage: 15.2 MB, less than 17.86% of Python3 online submissions for Find the Duplicate Number.
+"""
+
+
+"""
+Time Complexity: O(nlongn)
+Space Complexity: O(1)
 """
