dc3a7b8d18b2fdca5a1bc156cb42b412bf4a8cf6
Yi Cai <ceezyyy11@gmail.com>
2020-03-03 16:21:27 +0800

    Add files via upload

--- Python3/094.Binary Tree Inorder Traversal.py
+++ Python3/094.Binary Tree Inorder Traversal.py
@@ -1,7 +1,7 @@
 """
 @Title: 094. Binary Tree Inorder Traversal
 @Tag: binary tree
-@Date: Jan-13 2020
+@Date: Feb-03 2020
 @Author: ceezyyy
 @Difficulty: Medium
 """
@@ -15,25 +15,28 @@
 #         self.right = None
 
 
-# Solution 1: recursion
-
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        res = []
-        self.inorder(root, res)
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the first element from the stack
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:
+                    stack.append((node.right, False))
+                    stack.append((node, True))  # as the "root"
+                    stack.append((node.left, False))
         return res
-
-    def inorder(self, node: TreeNode, res: List[int]) -> None:
-        if not node:
-            return
-        if node.left:
-            self.inorder(node.left, res)
-        res.append(node.val)
-        if node.right:
-            self.inorder(node.right, res)
 
 
 """
-Runtime: 20 ms, faster than 96.79% of Python3 online submissions for Binary Tree Inorder Traversal.
+Runtime: 24 ms, faster than 87.53% of Python3 online submissions for Binary Tree Inorder Traversal.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Binary Tree Inorder Traversal.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
