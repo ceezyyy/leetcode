3dccd1a3ffe4fb31e8d568b7dfb2b64e87fb1dab
Yi Cai <ceezyyy11@gmail.com>
2020-03-05 11:43:41 +0800

    Add files via upload

--- Python3/145.Binary Tree Postorder Traversal.py
+++ Python3/145.Binary Tree Postorder Traversal.py
@@ -1,10 +1,13 @@
 """
 @Title: 145. Binary Tree Postorder Traversal
 @Tag: binary tree
-@Date: Mar-03 2020
+@Date: Mar-05 2020
 @Author: ceezyyy
 @Difficulty: Hard
 """
+
+
+# Approach 1: (Iterative)
 
 
 # Definition for a binary tree node.
@@ -13,7 +16,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
@@ -32,6 +34,41 @@ class Solution:
 
 """
 Runtime: 24 ms, faster than 87.85% of Python3 online submissions for Binary Tree Postorder Traversal.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Binary Tree Postorder Traversal.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+
+# Approach 2: (Recursive)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, node, res):
+        if node:  # left -> right -> root
+            self.dfs(node.left, res)
+            self.dfs(node.right, res)
+            res.append(node.val)
+
+
+"""
+Runtime: 24 ms, faster than 87.75% of Python3 online submissions for Binary Tree Postorder Traversal.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Binary Tree Postorder Traversal.
 """
 
