b90e009b4e8cc011c24e043e7e17beecafe21028
Yi Cai <ceezyyy11@gmail.com>
2020-03-05 11:35:54 +0800

    Add files via upload

--- Python3/094.Binary Tree Inorder Traversal.py
+++ Python3/094.Binary Tree Inorder Traversal.py
@@ -7,6 +7,9 @@
 """
 
 
+# Approach 1: (Iterative)
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -32,6 +35,42 @@ class Solution:
 
 """
 Runtime: 24 ms, faster than 87.53% of Python3 online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Binary Tree Inorder Traversal.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+
+# Approach 2: (Recursive)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, node, res):
+        if node:
+            self.dfs(node.left, res)
+            res.append(node.val)
+            self.dfs(node.right, res)
+
+
+"""
+Runtime: 20 ms, faster than 97.04% of Python3 online submissions for Binary Tree Inorder Traversal.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Binary Tree Inorder Traversal.
 """
 
