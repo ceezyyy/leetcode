3b966afd3d195f89c671b0c2947d62b2ebf74bb6
Yi Cai <ceezyyy11@gmail.com>
2020-03-06 16:35:23 +0800

    Add files via upload

--- Python3/515.Find Largest Value in Each Tree Row.py
+++ Python3/515.Find Largest Value in Each Tree Row.py
@@ -1,39 +1,46 @@
 """
 @Title: 515. Find Largest Value in Each Tree Row
 @Tag: binary tree
-@Date: Oct-28 2019
+@Date: Mar-06 2020
 @Author: ceezyyy
 @Difficulty: Medium
 """
 
-from typing import List
 
 # Definition for a binary tree node.
-
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Solution:
     def largestValues(self, root: TreeNode) -> List[int]:
-        # level order
-        res = []
         if not root:
-            return res
-        queue = [root]
-        while queue:
-            temp = []
-            size = len(queue)
-            for i in range(size):  # each level
-                node = queue.pop(0)  # the front of the queue
-                if(node.left):  # left child
+            return []
+        res, queue = [], [root]
+        while queue:  # BFS
+            n = len(queue)
+            temp = float("-inf")
+            for i in range(n):
+                node = queue.pop(0)
+                temp = node.val if node.val > temp else temp  # keep tracking the max
+                if node.left:
                     queue.append(node.left)
-                if(node.right):  # right child
+                if node.right:
                     queue.append(node.right)
-                temp.append(node.val)
-            res.append(max(temp))
+            res.append(temp)
         return res
+
+
+"""
+Runtime: 40 ms, faster than 92.57% of Python3 online submissions for Find Largest Value in Each Tree Row.
+Memory Usage: 15 MB, less than 100.00% of Python3 online submissions for Find Largest Value in Each Tree Row.
+"""
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
