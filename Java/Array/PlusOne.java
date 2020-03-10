75e49093143fe2faa7f9189a93b7fc225aecb9ed
Yi Cai <ceezyyy11@gmail.com>
2020-03-10 16:04:26 +0800

    Add files via upload

---
+++ Java/Array/PlusOne.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] plusOne(int[] A) {
+        for (int i = A.length - 1; i >= 0; i--) {
+            /*
+             * From the very beginning
+             * if current bit is less than 9
+             * plus one and return the digit immediately
+             * else set it as zero, then continue
+             */
+            if (A[i] < 9) {
+                A[i]++;
+                return A;
+            }
+            A[i] = 0;
+        }
+        /*
+         * Add one new digit
+         */
+        int[] res = new int[A.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
+
