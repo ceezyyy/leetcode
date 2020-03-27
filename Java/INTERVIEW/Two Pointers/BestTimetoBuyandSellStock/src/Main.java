82c23041ff39bcb8fe57e3dd5048d499fa258612
ceezyyy <ceezyyy11@gmail.com>
2020-03-27 09:18:08 +0800

    Update Two Pointers

---
+++ Java/INTERVIEW/Two Pointers/BestTimetoBuyandSellStock/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = solution.maxProfit(prices);
+        System.out.println(result);
+    }
+}
