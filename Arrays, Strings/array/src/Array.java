eb67c61bd6830a0e5745670bbd32702cf7d698b0
ceezyyy <ceezyyy11@gmail.com>
2020-11-02 18:11:12 +0800

    Update array

--- Arrays, Strings/array/src/Array.java
+++ Arrays, Strings/array/src/Array.java
@@ -6,7 +6,10 @@
 public class Array {
 
     private int[] data;
+
+    // the number of elements
     private int n;
+
     private int capacity;
 
     /**
@@ -18,6 +21,7 @@ public class Array {
         int[] data = new int[capacity];
         this.data = data;
         this.capacity = capacity;
+        this.n = 0;
     }
 
     /**
@@ -26,8 +30,8 @@ public class Array {
      * @param index
      * @return
      */
-    public int findByIndex(int index) {
-        if (index < 0 && index > capacity) return -1;
+    public int get(int index) {
+        if (index < 0 || index >= n) return -1;
         return data[index];
     }
 
@@ -39,8 +43,29 @@ public class Array {
      * @return
      */
     public boolean insert(int index, int value) {
-        if (index < 0 && index > capacity) return false;
+        if (index < 0 || index > n || n == capacity) return false;
+        // move elements behind index
+        for (int i = n; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = value;
+        // Do not forget to increase n by one
+        n++;
+        return true;
+    }
 
+
+    /**
+     * Insert an element
+     *
+     * @param value
+     * @return
+     */
+    public boolean insert(int value) {
+        if (n >= capacity) return false;
+        data[n] = value;
+        // Do not forget to increase n by one
+        n++;
         return true;
     }
 
@@ -51,6 +76,14 @@ public class Array {
      * @return
      */
     public boolean delete(int index) {
+        if (index < 0 || index >= n) return false;
+        for (int i = index + 1; i < n; i++) {
+            data[i - 1] = data[i];
+        }
+        // Do not forget to decrease n by one
+        --n;
+        // zero represent deleted element
+        data[n] = 0;
         return true;
     }
 
@@ -59,13 +92,24 @@ public class Array {
      */
     public void printAll() {
         for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+            System.out.print(data[i]);
+            System.out.print(" ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-
-
+        Array myArray = new Array(5);
+        // add elements
+        for (int i = 0; i < 10; i++) {
+            myArray.insert(0, i + 1);
+        }
+        myArray.printAll();  // 5 4 3 2 1
+        // remove elements
+        for (int i = 0; i < 5; i++) {
+            myArray.delete(0);
+        }
+        myArray.printAll();  // 0 0 0 0 0
     }
 
 }
