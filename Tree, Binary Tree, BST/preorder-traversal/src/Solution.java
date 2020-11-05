5e67251075c34b856511c26154c4bdf7bcb79379
ceezyyy <ceezyyy11@gmail.com>
2020-11-05 12:01:04 +0800

    Add binary tree / preorder traversal

---
+++ Tree, Binary Tree, BST/preorder-traversal/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Preorder traversal of binary tree
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    private List<Integer> result = new LinkedList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        pre(root);
+        return result;
+    }
+
+    public void pre(TreeNode node) {
+        if (node == null) return;
+        result.add(node.val);
+        pre(node.left);
+        pre(node.right);
+    }
+
+} 
