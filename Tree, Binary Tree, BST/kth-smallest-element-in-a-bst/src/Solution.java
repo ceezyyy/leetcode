0211d3478315574d37bd172715d34acbc76454da
ceezyyy <ceezyyy11@gmail.com>
2020-11-19 15:45:44 +0800

    Add Kth Smallest Element in a BST

--- Tree, Binary Tree, BST/kth-smallest-element-in-a-bst/src/Solution.java
+++ Tree, Binary Tree, BST/kth-smallest-element-in-a-bst/src/Solution.java
@@ -1,71 +1,6 @@
-# BST
+import java.util.ArrayList;
+import java.util.List;
 
-Table of Contents
------------------
-
-* [Overview](#overview)
-* [Kth Smallest Element in a BST](#kth-smallest-element-in-a-bst)
-
-
-
-## Overview
-
-
-
-
-
-
-
-## Kth Smallest Element in a BST
-
-**Example 1**
-
-```
-Input: root = [3,1,4,null,2], k = 1
-   3
-  / \
- 1   4
-  \
-   2
-Output: 1
-```
-
-**Example 2**
-
-```
-Input: root = [5,3,6,2,4,null,null,1], k = 3
-       5
-      / \
-     3   6
-    / \
-   2   4
-  /
- 1
-Output: 3
-```
-
-
-
-**Follow up**
-What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
-
-
-
-**Explained**
-
-
-
-
-
-
-
-
-
-
-
-**Solution.java**
-
-```java
 /**
  * Kth Smallest Element in a BST
  */
@@ -97,4 +32,3 @@ class Solution {
         inorder(root.right);
     }
 }
-```
