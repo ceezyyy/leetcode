cdea6c6effed82b8f42317365bd12fa5756097d5
ceezyyy <ceezyyy11@gmail.com>
2020-11-24 18:17:17 +0800

    Add Count Sorted Vowel Strings

--- Recursion & Backtracking/count-sorted-vowel-strings/src/Solution.java
+++ Recursion & Backtracking/count-sorted-vowel-strings/src/Solution.java
@@ -1,24 +1,6 @@
-# Practices for Backtracking
+import java.util.ArrayList;
+import java.util.List;
 
-## Count Sorted Vowel Strings
-
-**Example**
-
-```
-Input: n = 2
-Output: 15
-Explanation: The 15 sorted strings that consist of vowels only are
-["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
-Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
-```
-
-**Explained**
-
-
-
-**Solution.java**
-
-```java
 /**
  * Count Sorted Vowel Strings
  */
@@ -57,4 +39,3 @@ class Solution {
         }
     }
 }
-```
