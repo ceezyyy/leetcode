88f0dba9a7f162565df241aff84909bd891fd8f2
ceezyyy <ceezyyy11@gmail.com>
2020-11-28 18:07:51 +0800

    Add Generate Parentheses

---
+++ Recursion & Backtracking/generate-parentheses/src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Generate Parentheses
+ */
+class Solution {
+
+    private List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        // 1 <= n <= 8
+        backtracking(n, n, new StringBuilder());
+        return result;
+    }
+
+    public void backtracking(int left, int right, StringBuilder runningChoices) {
+
+        // Goal: We have made all decisions
+        if (left == 0 && right == 0) {
+            result.add(new String(runningChoices));
+            return;
+        }
+
+        // Decision tree traversal
+        if (left > 0) {
+            runningChoices.append("(");
+            backtracking(left - 1, right, runningChoices);
+            runningChoices.deleteCharAt(runningChoices.length() - 1);
+        }
+
+        // e.g: "())" is not a valid parentheses
+        if (right > left) {
+            runningChoices.append(")");
+            backtracking(left, right - 1, runningChoices);
+            runningChoices.deleteCharAt(runningChoices.length() - 1);
+        }
+
+    }
+}
